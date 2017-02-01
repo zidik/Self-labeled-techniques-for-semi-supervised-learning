c5c7597cdc8be696b881b2fecd64cd59b5cf703e
Mark Laane <MarkLaane@gmail.com>
2017-02-01 21:29:55 +0100

    fixed standard self-training and introduced some confusion matrixes

--- standard_self_training.py
+++ standard_self_training.py
@@ -1,3 +1,4 @@
+import numpy as np
 class StandardSelfTraining:
     def __init__(self, name, base_classifier, max_iterations=40):
         self.name = name
@@ -8,23 +9,30 @@ class StandardSelfTraining:
         return "Classifier: " + self.name + "\nParameters: " + str(self.base_classifier.get_params())
 
     def fit(self, X, y):
-        stable = False
+        y = np.copy(y)#copy in order not to change original data
+        
+        all_labeled = False
         iteration = 0
         # Iterate until the result is stable or max_iterations is reached
-        while not stable and (iteration < self.max_iterations):
-            new_y = self._fit_iteration(X, y)
-            # Check if the result has stabilised
-            stable = (y == new_y).all()
-            y = new_y
+        while not all_labeled  and (iteration < self.max_iterations):
+            self._fit_iteration(X, y)
+            all_labeled = (y != "unlabeled").all()
             iteration += 1
+        print(iteration,end="")
 
     def _fit_iteration(self, X, y):
+        threshold = 0.7
+        
         clf = self.base_classifier
         # Fit a classifier on already labeled data
         labeled = y != "unlabeled"
         clf.fit(X[labeled], y[labeled])
-        # Predict on all the training data
-        return clf.predict(X)
+
+        probabilities = clf.predict_proba(X)
+        threshold = min(threshold, probabilities[~labeled].max()) #Get at least the best one
+        over_thresh = probabilities.max(axis=1)>=threshold
+        
+        y[~labeled & over_thresh] = clf.predict(X[~labeled & over_thresh])
 
     def predict(self, X):
         return self.base_classifier.predict(X)
